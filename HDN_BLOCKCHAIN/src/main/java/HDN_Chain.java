b5105725cfab179acff40aef3b37f68562c97985
HoangDucNgu <hodung28@gmail.com>
2022-10-03 21:04:23 +0700

    Commit 2

--- HDN_BLOCKCHAIN/src/main/java/HDN_Chain.java
+++ HDN_BLOCKCHAIN/src/main/java/HDN_Chain.java
@@ -1,25 +1,65 @@
 import com.google.gson.GsonBuilder;
-
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class HDN_Chain {
     public static ArrayList<Block> blockchain = new ArrayList<Block>();
     public static int difficulty = 5;
     public static void main(String[] args) {
         //add our blocks to the blockchain ArrayList:
-        blockchain.add(new Block("Hi im the first block", "0"));
-        System.out.println("Trying to Mine block 1... ");
-        blockchain.get(0).mineBlock(difficulty);
-        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 2... ");
-        blockchain.get(1).mineBlock(difficulty);
-        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
-        System.out.println("Trying to Mine block 3... ");
-        blockchain.get(2).mineBlock(difficulty);
-        System.out.println("\nBlockchain is Valid: " + isChainValid());
-        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
-        System.out.println("\nThe block chain: ");
-        System.out.println(blockchainJson);
+        Scanner BlockData = new Scanner(System.in);
+
+        System.out.println("Hãy nhập số lượng khối của chuỗi: ");
+        int i = Integer.parseInt(BlockData.nextLine());
+        //int i = BlockData.nextInt();
+        if (i > 0)
+        {
+            for (int j = 0; j < i; j++) {
+                if (j == 0) {
+                    System.out.println("Khởi tạo thông tin khối đầu tiên!");
+                    String data = BlockData.nextLine();
+                    blockchain.add(new Block(data, "0"));
+                    System.out.println("Trying to Mine block 0... ");
+                    blockchain.get(j).mineBlock(difficulty);
+                } else {
+                    System.out.println("Nhập dữ liệu của khối thứ: " + j);
+                    System.out.println("Nhập họ và tên: ");
+                    String hoten = BlockData.nextLine();
+                    System.out.println("Nhập mã hợp đồng: ");
+                    String maHD = BlockData.nextLine();
+                    //String chuoi= hoten.concat(maHD);
+                    String chuoi = (new StringBuilder()).append(hoten).append("-").append(maHD).toString();
+                    blockchain.add(new Block(chuoi, blockchain.get(blockchain.size() - 1).hash));
+                    System.out.println("Trying to Mine block " + j);
+                    blockchain.get(j).mineBlock(difficulty);
+                }
+            }
+            System.out.println("\nBlockchain is Valid: " + isChainValid());
+            String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+            System.out.println("\nThe block chain: ");
+            System.out.println(blockchainJson);
+        }
+            else
+        {
+            System.out.printf("SỐ KHỐI PHẢI LỚN HƠN 0");
+        }
+
+        //blockchain.add(new Block("Hi im the first block", "0"));
+        //System.out.println("Trying to Mine block 1... ");
+        //blockchain.get(0).mineBlock(difficulty);
+
+        //blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        //System.out.println("Trying to Mine block 2... ");
+        //blockchain.get(1).mineBlock(difficulty);
+
+        //blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        //System.out.println("Trying to Mine block 3... ");
+        //blockchain.get(2).mineBlock(difficulty);
+
+        //System.out.println("\nBlockchain is Valid: " + isChainValid());
+        //String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        //System.out.println("\nThe block chain: ");
+        //System.out.println(blockchainJson);
     }
     //Phương thức kiểm tra tinh toàn vẹn của Blockchain
     public static Boolean isChainValid() {
